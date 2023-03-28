c4625184d80681f92249b599d668e35f03522484
Oğuz Karan <oguzkaran@csystem.org>
2023-03-28 20:41:13 +0300

    Update

---
+++ src/Projects/003-MavenRemoteRepositoryUsageApp/src/main/java/org/csystem/app/libraryusage/App.java
@@ -0,0 +1,8 @@
+package org.csystem.app.libraryusage;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Application.run(args);
+    }
+}
---
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/App.java
@@ -0,0 +1,10 @@
+package org.csystem.app;
+
+import com.karandev.util.console.Console;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Application.run(args);
+    }
+}
