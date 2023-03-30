c972553a1c6550ebc4f98a6787e98f549f0904d4
Oğuz Karan <oguzkaran@csystem.org>
2023-03-30 21:40:47 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,12 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    Console sınıfının readBigDecimal metotları
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import com.karandev.util.console.Console;
 
 class Application {
     public static void run(String[] args)
     {
+        var a = Console.readBigDecimal("Birinci sayıyı giriniz:");
+        var b = Console.readBigDecimal("İkinci sayıyı giriniz:");
+        var c = Console.readBigDecimal("Üçüncü sayıyı giriniz:");
+        var d = a.add(b);
 
+        Console.writeLine("a = %.20f", a.doubleValue());
+        Console.writeLine("b = %.20f", b.doubleValue());
+        Console.writeLine("c = %.20f", c.doubleValue());
+        Console.writeLine("d = %.20f", d.doubleValue());
+        Console.writeLine(c.equals(d) ? "Eşit" : "Eşit değil");
     }
 }
-
