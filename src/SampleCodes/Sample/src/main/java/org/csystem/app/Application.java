411950288f007f83b94b139fc983f2178ff77491
Oğuz Karan <oguzkaran@csystem.org>
2023-05-09 22:50:40 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,23 +3,9 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import com.karandev.util.console.Console;
-
 class Application {
     public static void run(String [] args)
     {
-        Sample.foo(10L, 10L); //error: uygun (applicable) metot yok
-    }
-}
 
-class Sample {
-    public static void foo(long a, int...b)//#1
-    {
-        Console.writeLine("foo, long, int...");
-    }
-
-    public static void foo(int a, long...b) //#2
-    {
-        Console.writeLine("foo, int, long...");
     }
 }
