e861b28c2854a94bf1b88e8b24f4ed7ee1334afc
Oğuz Karan <oguzkaran@csystem.org>
2023-05-12 01:41:55 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,6 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örnekte kendini çağıran metot bu işlemi sürekli yaptığından StackOverflowError nesnesi fırlatılır. Metodun
-    parametre değişkeni ve yerel değişkeni olmadığına dikkat ediniz
+
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
