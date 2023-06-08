d06f6adf6365aeaf581b0ea42c12c8c25977c02f
Oğuz Karan <oguzkaran@csystem.org>
2023-06-08 21:08:28 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,25 +1,42 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    ~ operatörü tek operandlı (unary), önek (prefix) durumunda bir operatördür. Bu operatör operandına ilişkin
-    ifadenin değeri olan tamsayının bitleri üzerinde 1'e tümeleme (one's complement) işlemi yaparak elde edilen değeri
-    üretir. Yani operandı olan tamsayının 1 olan bitlerini sıfır, sıfır olan bitlerini de 1 yapar
+    
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import com.karandev.util.console.Console;
-import org.csystem.util.bitwise.BitwiseUtil;
 
 class Application {
     public static void run(String [] args)
     {
         while (true) {
-            var val = Console.readInt("Bir sayı giriniz:");
-            var result = ~val;
+            var n = Console.readInt("Bir sayı giriniz:");
 
-            BitwiseUtil.writeBits(val);
-            BitwiseUtil.writeBits(result);
-
-            if (val == 0)
+            if (n == 0)
                 break;
+
+            var str = Console.read("Bir yazı giriniz:");
+            var encStr = CryptoUtil.encryptDecrypt(str, n);
+
+            Console.writeLine(encStr);
+
+            var decStr = CryptoUtil.encryptDecrypt(encStr, n);
+
+            Console.writeLine(decStr);
         }
     }
 }
+
+
+class CryptoUtil {
+    public static String encryptDecrypt(String s, int n)
+    {
+        var len = s.length();
+        var sb = new StringBuilder(n);
+
+        for (var i = 0; i < len; ++i)
+            sb.append((char)(s.charAt(i) ^ n));
+
+        return sb.toString();
+    }
+}
+
