6ba7aeebd6b70288363c85f1a70e4fd8f768e319
Oğuz Karan <oguzkaran@csystem.org>
2023-06-15 22:30:55 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,69 +1,33 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Bir metot içerisinde bildirilen sınıflara yerel sınıflar denir. Yerel sınıflar adında da anlaşılacağı gibi yalnızca
-    bildirildikleri blok boyunca görülebilirdirler. Yerel sınıflar pratikte çok fazla kullanılmaz. Yerel sınıflar byte
-    kodları oluşturulurken genel olarak şu isimlendirme şablonu kullanılır:
-        <kapsayan sınıf ismi>$<yukarıdan aşağıya kaçıncı aynı isimde yerel sınıf olduğu bilgisi><yerel sınıf ismi>
-    Aşağıdaki örnekte yerel sınıfların arakodları A$1B.class ve A$2B.class şeklinde üretilecektir
+    static bir sınıf ismine import static bildirimleri ile de doğrudan erişilebilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import com.karandev.util.console.Console;
 
+import static org.csystem.app.A.B;
+
 class Application {
     public static void run(String [] args)
     {
-        A a = new A();
+        var b = new B();
 
-        a.tar(20);
+        b.foo(10);
     }
-}
-
-class B {
-    //...
 }
 
 class A {
-    public void tar(int val)
-    {
-        class B {
-            public static void foo()
-            {
-                Console.writeLine("B.foo");
-            }
+    private int m_value;
+    //...
+    public static class B {
+        public void foo(int value)
+        {
+            var x = new A();
 
-            public void bar()
-            {
-                Console.writeLine("B.bar");
-            }
+            x.m_value = value;
+
+            Console.writeLine("m_value = %d", x.m_value);
         }
-
-        B.foo();
-
-        B x = new B();
-
-        x.bar();
-    }
-
-    public void car(int val)
-    {
-        class B {
-            public static void foo()
-            {
-                Console.writeLine("B.foo");
-            }
-
-            public void bar()
-            {
-                Console.writeLine("B.bar");
-            }
-        }
-
-        B.foo();
-
-        B x = new B();
-
-        x.bar();
     }
 }
-
 
