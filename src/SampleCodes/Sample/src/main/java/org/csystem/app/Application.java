a2623cb09fa9dd44804b5ccb3fd1eee963183ca8
Oğuz Karan <oguzkaran@csystem.org>
2023-07-04 22:34:26 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,18 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Anonim Sınıflar: Bildirim sırasında programcının isim vermediği sınıflardır. Anonim sınıf bildiriminin genel biçimi
-    şu şekildedir:
-        new <tür>([Argümanlar]) {
-            <bildirimler>
-        }
-    Bu sentaksta tür sınıf, arayüz vb. user defined type'lardan olabilir. Eğer bu tür bir sınıf ise o sınıftan türetilmiş
-    bir sınıf bildirimi ile beraber, türemiş sınıf türünden nesne yaratılmış olur. Eğer tür bir arayüz ise o arayüzü
-    destekleyen (implementation) bir sınıf bildirimi ve o sınıf türünden bir nesne yatılmış olur. Şüphesiz anonim sınıflara
-    byte code üretilirken isim verilir. Bu ismin genel convention'ı şudur:
-        <anonim sınıf bildiriminin yapıldığı sınıf ismi>$<o sınıf içerisinde yukarıdan aşağıya kaçıncı anonim sınıf olduğu sayısı>
-    Birebir aynı olsa bile her bildirim ayrı bir sınıf demektir.
-    Bu sentaksta kullanılan sınıfın türetmeye kapalı olmaması yani final olarak bildirilmemiş olması gerekir. Sınıfn
-    sentaksta geçilen argümanlar için "the most applicable" ctor'unun var olması gerekir. Yani hangi ctor'un çağrılacağı
-    method overload resolution kurallarına göre belirlenir
+    
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -21,40 +8,78 @@ import com.karandev.util.console.Console;
 class Application {
     public static void run(String [] args)
     {
-        Sample s1 = new Sample() {
+        var a = Console.readInt("Birinci sayıyı giriniz:");
+        var b = Console.readInt("İkinci sayıyı giriniz:");
+        var c = Console.readInt("Üçüncü sayıyı giriniz:");
 
-            public void foo()
+        IntValue iVal = new IntValue(a) {
+            public int addWith()
             {
-                //...
+                return val + b + c;
             }
-            //...
         };
 
-        Sample s2 = new Sample(10) {
-            //...
-        };
+        Console.writeLine(iVal.getClass().getName());
+        Util.printSum(iVal);
+        Util.printSum(new IntValue(a) {
+            public int addWith()
+            {
+                return val + b;
+            }
+        });
 
-        Console.writeLine(s1.getClass().getName());
-        Console.writeLine(s2.getClass().getName());
+        Console.writeLine("---------------------------------------");
+        var myValue1 = new MyValue1(a, b, c);
+        var myValue2 = new MyValue2(a, b);
+
+        Util.printSum(myValue1);
+        Util.printSum(myValue2);
     }
 }
 
+class MyValue1 extends IntValue {
+    private final int m_b, m_c;
 
-class Sample {
-    private int m_x;
-
-    public Sample()
+    public MyValue1(int a, int b, int c)
     {
+        super(a);
+        m_b = b;
+        m_c = c;
     }
 
-    public Sample(int x)
+    public int addWith()
     {
-        m_x = x;
-    }
-
-    public void printValue()
-    {
-        Console.writeLine("value: %d", m_x);
+        return val + m_b + m_c;
     }
 }
 
+class MyValue2 extends IntValue {
+    private final int m_b;
+    public MyValue2(int a, int b)
+    {
+        super(a);
+        m_b = b;
+    }
+    public int addWith()
+    {
+        return val + m_b;
+    }
+}
+
+class Util {
+    public static void printSum(IntValue iVal)
+    {
+        Console.writeLine(iVal.addWith());
+    }
+}
+
+abstract class IntValue {
+    protected int val;
+
+    protected IntValue(int val)
+    {
+        this.val = val;
+    }
+
+    public abstract int addWith();
+}
