a44c75b0499d981d46b570dc6f458b02883af151
Oğuz Karan <oguzkaran@csystem.org>
2023-07-18 23:59:25 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,18 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: SchedulerLib içerisinde bildirilen ve aşağıda açıklanan Scheduler isimli sınıfı yazınız.
-    Açıklamalar:
-        - Sınıf Time sınıfına benzemekle birlikte gecikme (delay) ve periyot (period) elemanlarını TimeUnit enum
-        sınıfı ile de alabilecektir
 
-        - Sınıf her periyotta yapptığı işi (callback) TimerTask yerine Runnable arayüzü ile alacaktır
-
-        - Sınıf ayrıca bir Runnable ile scheduler sonlandırılması (cancel) durumunda ne yapacağı bilgisinine alabilecektir.
-
-        - Bir scheduler ile tek bir schedule yapılabilecektir
-
-        - Sınıfın public bölümünü değiştirmedenn istediğiniz elemanı ekleyebilirsiniz
-
-     Açıklama: Sınıf kütüphanenin 14.1.0 versiyonunda eklenmiştir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
