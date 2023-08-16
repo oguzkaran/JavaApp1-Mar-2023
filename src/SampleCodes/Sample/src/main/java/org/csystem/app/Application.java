8977c99e80ea3101a4b087163980ed44135c63c1
Oğuz Karan <oguzkaran@csystem.org>
2023-08-16 09:25:57 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,25 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Deprecated annotation'ı bir bildirimin deprecated olarak işaretlenmesi için kullanılır. Deprecated, bir RUNTIME
-    annotation olmasına karşın Java derleyicisi ve bir çok static kod analizi aracı tarafından dikkate alınır. Java
-    derleyicileri ve static kod analizi araçları deprecated olan bir bildirimin kullanılması durumunda uyarı mesajı
-    verirler. Java 9 ile birlikte Deprecated annotation'ına boolean türden forRemoval ve String türden since elemanları
-    eklenmiştir. forRemoval default olarak false değerindedir. Bu eleman deprecated olan bildirimin ileride silinip
-    silinmeyeceği konusunda bilgi vermek amaçlı kullanılır. since elemanının default değeri boş string'dir. Genel olarak
-    deprecated olan versiyonu belirtmek için kullanılır. Derleyicilerin ve static kod analizi araçlarının hemen hepsi
-    bu iki eleman için de uygun uyarıyı verirler.
-    org-csystem-util-datetime-legacy kütüphanesindeki sınıfları inceleyiniz
+    
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import com.karandev.util.console.Console;
-import org.csystem.util.datetime.DateTime;
 
 class Application {
     public static void run(String [] args)
     {
-        var a = new Integer(10);
 
-        Console.writeLine(a);
     }
 }
 
