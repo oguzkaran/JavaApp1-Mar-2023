6b8fccfd83512c8d8bd994a865b158912e0e1826
Oğuz Karan <oguzkaran@csystem.org>
2023-08-24 02:19:59 +0300

    Update

--- src/Libraries/JavaLibs/CSDLibs/MathLib/src/main/java/org/csystem/math/Fraction.java
+++ src/Libraries/JavaLibs/CSDLibs/MathLib/src/main/java/org/csystem/math/Fraction.java
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------
 	FILE        : Fraction.java
 	AUTHOR      : JavaApp1-Mar-2023 Group
-	LAST UPDATE : 15.08.2023
+	LAST UPDATE : 23.08.2023
 
 	Fraction class that represents a fraction
 
@@ -38,9 +38,9 @@ public class Fraction {
     {
         if (b == 0) {
             if (a == 0)
-                throw new OperationStatusException("Indeterminate", OperationStatus.INDETERMINATE);
+                throw new NumberFormatException("Indeterminate");
 
-            throw new OperationStatusException("Undefined", OperationStatus.UNDEFINED);
+            throw new NumberFormatException("Undefined");
         }
     }
 
