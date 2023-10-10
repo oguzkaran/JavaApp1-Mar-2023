d69939a8b41e07f8df4b1047823a869d58f64bd4
Oğuz Karan <oguzkaran@csystem.org>
2023-10-10 22:23:17 +0300

    Update

---
+++ src/Libraries/JavaLibs/CSDLibs/CollectionLib/src/main/java/org/csystem/collection/CSDBoundedStack.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------
+	FILE        : CSDBoundedStack.java
+	AUTHOR      : JavaApp1-Mar-2023 Group
+	LAST UPDATE : 10.10.2023
+
+	CSDStack class
+
+	Copyleft (c) 1993 by C and System Programmers Association (CSD)
+	All Rights Free
+-----------------------------------------------------------------------*/
+package org.csystem.collection;
+
+public class CSDBoundedStack<E>  {
+    private final E [] m_items;
+
+    public CSDBoundedStack(int size)
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+
+    public boolean empty()
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+
+    public E peek()
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+
+    public E pop()
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+
+    public E push(E item)
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+
+    public int search(Object obj)
+    {
+        throw new UnsupportedOperationException("Not yet implemented!...");
+    }
+}
---
+++ src/Libraries/JavaLibs/CSDLibs/CollectionLib/src/main/java/org/csystem/collection/FullStackException.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------------------------
+	FILE        : FullStackException.java
+	AUTHOR      : JavaApp1-Mar-2023 Group
+	LAST UPDATE : 10.10.2023
+
+	FullStackException class
+
+	Copyleft (c) 1993 by C and System Programmers Association (CSD)
+	All Rights Free
+-----------------------------------------------------------------------*/
+package org.csystem.collection;
+
+public class FullStackException extends RuntimeException {
+
+}
