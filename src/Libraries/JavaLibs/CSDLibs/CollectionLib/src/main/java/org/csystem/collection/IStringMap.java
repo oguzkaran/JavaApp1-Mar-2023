a085558b04aedb05634f1a1d0c906bd3b285d826
Oğuz Karan <oguzkaran@csystem.org>
2023-11-16 13:56:20 +0300

    Update

---
+++ src/Libraries/JavaLibs/CSDLibs/CollectionLib/src/main/java/org/csystem/collection/IStringMap.java
@@ -0,0 +1,15 @@
+package org.csystem.collection;
+
+import java.util.Optional;
+
+public interface IStringMap<T> {
+    int count();
+
+    boolean addElement(String key, T value);
+
+    boolean removeElement(String key);
+
+    Optional<T> getValue(String key);
+
+    T getValue(String key, T defaultValue);
+}
---
+++ src/Libraries/JavaLibs/CSDLibs/CollectionLib/src/main/java/org/csystem/collection/StringMap.java
@@ -0,0 +1,35 @@
+package org.csystem.collection;
+
+import java.util.Optional;
+
+public class StringMap<T> implements IStringMap<T> {
+    @Override
+    public int count()
+    {
+        throw new UnsupportedOperationException("Not implemented yet!...");
+    }
+
+    @Override
+    public boolean addElement(String key, T value)
+    {
+        throw new UnsupportedOperationException("Not implemented yet!...");
+    }
+
+    @Override
+    public boolean removeElement(String key)
+    {
+        throw new UnsupportedOperationException("Not implemented yet!...");
+    }
+
+    @Override
+    public Optional<T> getValue(String key)
+    {
+        throw new UnsupportedOperationException("Not implemented yet!...");
+    }
+
+    @Override
+    public T getValue(String key, T defaultValue)
+    {
+        throw new UnsupportedOperationException("Not implemented yet!...");
+    }
+}
