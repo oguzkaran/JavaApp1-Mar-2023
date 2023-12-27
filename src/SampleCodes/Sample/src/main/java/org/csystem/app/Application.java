7f6c686874fd5508a0b6d8f5b3a38ee2e92862d7
Oğuz Karan <oguzkaran@csystem.org>
2023-12-27 16:27:07 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -4,29 +4,48 @@
 package org.csystem.app;
 
 import com.karandev.io.util.console.Console;
-import org.csystem.util.data.test.factory.StudentFactory;
-import org.csystem.util.data.test.student.StudentInfo;
+import org.csystem.util.data.test.factory.ProductFactory;
+import org.csystem.util.data.test.product.ProductInfo;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 import static com.karandev.io.util.console.commandline.CommandLineUtil.checkLengthEquals;
-import static java.util.stream.Collectors.groupingBy;
 
 class Application {
+    private static void writeProducts(List<ProductInfo> products, String path) throws IOException
+    {
+        try (var bw = Files.newBufferedWriter(Path.of(path), StandardCharsets.UTF_8, StandardOpenOption.CREATE)) {
+            products.forEach(p -> writeProduct(p, bw));
+        }
+    }
+
+    private static void writeProduct(ProductInfo productInfo, BufferedWriter bw)
+    {
+        try {
+            bw.write(productInfo.toString() + "\r\n");
+        }
+        catch (IOException ex) {
+            Console.Error.writeLine("IO error occurs:%s", ex.getMessage());
+        }
+    }
+
     public static void run(String[] args)
     {
         checkLengthEquals(args.length, 2, "Wrong number of arguments");
 
         try {
-            var grade = Integer.parseInt(args[1]);
-            var factory = StudentFactory.loadFromTextFile(Path.of(args[0]));
-            var students = factory.STUDENTS;
+            var factory = ProductFactory.loadFromTextFile(Path.of(args[0]));
+            var products = factory.getProductsAsIterable();
+            var a = StreamSupport.stream(products.spliterator(), false).filter(p -> p.getStock() > 0);
 
-            var map = students.stream().filter(s -> s.getMidtermGrade() * 0.4 + s.getFinalGrade() * 0.6 > grade)
-                    .collect(groupingBy(StudentInfo::getLecture));
-
-            map.keySet().forEach(lec -> {Console.writeLine(lec); map.get(lec).forEach(Console::writeLine);});
+            Console.writeLine(a.getClass().getName());
         }
         catch (NumberFormatException ignore) {
             Console.Error.writeLine("Invalid grade value!...");
