8d9c032d3c240da68601136442d3094d342fadda
Oğuz Karan <oguzkaran@csystem.org>
2024-07-08 01:42:19 +0300

    update spaces

--- src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoLocationSaveDTO.java
+++ src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoLocationSaveDTO.java
@@ -12,42 +12,24 @@ import lombok.experimental.Accessors;
 @Accessors(prefix = "m_")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class WeatherInfoLocationSaveDTO {
-
     private int m_elevation;
-
     private String m_observation;
-
     private String m_clouds;
-
     private String m_temperature;
-
     private int m_humidity;
-
     private String m_iCAO;
-
     @EqualsAndHashCode.Include
     public String dateTime;
-
     private double m_longitude;
-
     private double m_latitude;
-
     private String m_dewPoint;
-
     private String m_countryCode;
-
     @EqualsAndHashCode.Include
     private String m_stationName;
-
     private String m_weatherCondition;
-
     private int m_windDirection;
-
     private String m_windSpeed;
-
     private String m_cloudsCode;
-
     private int m_hectoPascAltimeter;
-
     private PlaceInfoLocation m_placeInfoLocation;
 }
