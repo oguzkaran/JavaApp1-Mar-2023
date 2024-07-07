d172f7fc0ec89cb43a7fc17ede731464d793997d
Oğuz Karan <oguzkaran@csystem.org>
2024-07-08 01:39:06 +0300

    update spaces

--- src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoLocationDTO.java
+++ src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoLocationDTO.java
@@ -23,35 +23,25 @@ public class WeatherInfoLocationDTO {
     private String m_iCAO;
     @EqualsAndHashCode.Include
     public String dateTime;
-
     @Accessors(prefix = "m_")
     private double m_longitude;
-
     @Accessors(prefix = "m_")
     private double m_latitude;
-
     @Accessors(prefix = "m_")
     private String m_dewPoint;
-
     @Accessors(prefix = "m_")
     private String m_countryCode;
-
     @Accessors(prefix = "m_")
     @EqualsAndHashCode.Include
     private String m_stationName;
-
     @Accessors(prefix = "m_")
     private String m_weatherCondition;
-
     @Accessors(prefix = "m_")
     private int m_windDirection;
-
     @Accessors(prefix = "m_")
     private String m_windSpeed;
-
     @Accessors(prefix = "m_")
     private String m_cloudsCode;
-
     @Accessors(prefix = "m_")
     private int m_hectoPascAltimeter;
 }
