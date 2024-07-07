5eec544a72d66ebae0bb90ec57d091d04ffdd2ff
Oğuz Karan <oguzkaran@csystem.org>
2024-07-08 00:51:20 +0300

    update

--- src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoRegionDTO.java
+++ src/Projects/011-SpringBoot/WeatherInfoService/WeatherInfoDailyServiceLib/src/main/java/com/sunny/app/weatherinfo/daily/service/dto/WeatherInfoRegionDTO.java
@@ -12,33 +12,20 @@ import lombok.experimental.Accessors;
 @Accessors(prefix = "m_")
 public class WeatherInfoRegionDTO {
     private double m_longitude;
-
     private String m_observation;
-
     private String m_iCAO;
-
     private String m_clouds;
-
     private String m_dewPoint;
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String m_cloudsCode;
-
     @EqualsAndHashCode.Include
     public String dateTime;
-
     private String m_temperature;
-
     private int m_humidity;
-
     @EqualsAndHashCode.Include
     private String m_stationName;
-
     private String m_weatherCondition;
-
     private int m_windDirection;
-
     private String m_windSpeed;
-
     private double m_latitude;
 }
